c4ccdd9cf4833c9b7d50e5fa2b446a03e4bb9ace
Douglas Figueiredo <douglasarts@gmail.com>
2021-08-30 16:42:22 -0300

    App de Produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
-import { TaskList } from './components/TaskList'
-import { Header } from "./components/Header";
-import './styles/global.scss'
+import React, { useState, useEffect } from 'react'
+import http from './services/AWSClient'
+import { Container, Card, Title, Info } from './styles/products'
+interface ProductProps {
+  productName: string;
+  items: [Items];
+}
+interface Items {
+  sellers: [Sellers]
+}
 
+interface Sellers {
+  sellerId: string;
+  commertialOffer: CommertialOffer
+}
+interface CommertialOffer{
+  Installments: [Installments];
+}
+interface Installments {
+  Value: Number;
+}
 
 export function App() {
-  return (
-    <>
-      <Header />
-      <TaskList />
-    </>
+  
+  // const [products, setProducts] = useState([]);
+  
+  const [products, setProducts] = useState<ProductProps[]>([])
+
+
+  useEffect(() => {
+    http
+      .get('/')
+      .then((response) => response.data)
+        .then((data) => {
+          setProducts(data)
+        }
+      )
+    }, [])
+
+  return(
+    <Container>
+     {
+      products.map((product)=>{
+        const price = product.items[0].sellers[0].commertialOffer.Installments[0].Value
+        return (
+          <Card key="{produtc.id}">
+            <Title>{product.productName}</Title>
+            <Info>R$ {price}</Info>
+          </Card>
+        )
+      })
+      }
+    </Container>
   )
 }
